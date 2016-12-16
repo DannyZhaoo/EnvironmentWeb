e8f2e5bb5b2d250b456c7de744e3274b3023ecce
DannyZhaoo <XiudanZhao@Outlook.com>
2016-12-16 17:09:48 +0800

    "add some plugins to detect some js and css errors and begin to write the fisrt page of this project."

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,20 @@ module.exports=function(grunt){
     grunt.initConfig({
         //获取package.json的信息
         pkg:grunt.file.readJSON("package.json"),
+        //concat插件的配置信息
+        concat:{
+            options:{
+                stripBanners:true,
+            },
+            cssConcat:{
+                src:['src/css/css1.css','src/css/'],
+                dest:'src/css/<%=pkg.name%>-<%=pkg.version%>.css'
+            },
+            jsConcat:{
+                src:"src/js/*.js",
+                dest:'src/js/<%=pkg.name%>-<%=pkg.version%>.js'
+            }
+        },
         //uglify插件的配置信息
         uglify: {
             options: {
@@ -11,13 +25,26 @@ module.exports=function(grunt){
           },
           files: {
               expand: true,
-              cwd: 'src/',
+              cwd: 'src/js/',
               src: ['*.js'],
               dest: 'build/js/',
               ext: '.min.js'
           }
         },
-
+        //jshint
+        jshint:{
+            options:{
+                jshintrc:'.jshint'
+            },
+            build:['Gruntfile.js','src/js/*.js']
+        },
+        //csslint
+        csslint:{
+            options:{
+                csslintrc:'csslint'
+            },
+            build:['src/css/*.css']
+        },
         //cssmin插件的配置信息
         cssmin: {
             options: {
@@ -25,7 +52,7 @@ module.exports=function(grunt){
           },
           files: {
               expand: true,
-              cwd: 'src/',
+              cwd: 'src/css/',
               src: ['*.css'],
               dest: 'build/css/',
               ext: '.min.css'
@@ -34,8 +61,8 @@ module.exports=function(grunt){
         //watch插件的配置信息
         watch: {
             scripts: {
-            files: ['src/*.js', 'src/*.css'], // 监控的文件目录
-            tasks: ['uglify', 'cssmin'], // 监控到文件发生变化之后执行的任务列表
+            files: ['src/js/*.js', 'src/css/*.css'], // 监控的文件目录
+            tasks: ['jshint', 'csslint'], // 监控到文件发生变化之后执行的任务列表
             options: {
                 spawn: false,
             },
@@ -43,11 +70,16 @@ module.exports=function(grunt){
     }
 });
 //告诉grunt我们将使用插件
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
+//grunt.loadNpmTasks('grunt-contrib-uglify');
+//grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+//grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-csslint');
+
+
 
 //告诉grunt当我们在终端中输入grunt时需要做些什么（注意先后顺序）
-grunt.registerTask('default', ['uglify', 'cssmin', 'watch']);
+grunt.registerTask('default', ['jshint','csslint','watch']);
 };
 
